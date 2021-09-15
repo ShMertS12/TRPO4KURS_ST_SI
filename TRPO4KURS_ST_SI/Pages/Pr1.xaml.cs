23228c899d2d3c560655b1537b1812e65bbe44e4
fufel <fufel@DESKTOP-21KRHLR>
2021-09-15 14:25:42 +0300

    Добавьте файлы проекта.

---
+++ TRPO4KURS_ST_SI/Pages/Pr1.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TRPO4KURS_ST_SI.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Pr1.xaml
+    /// </summary>
+    public partial class Pr1 : Page
+    {
+        public Pr1()
+        {
+            InitializeComponent();
+        }
+
+        private void LogBut_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(TLB.Text) || string.IsNullOrEmpty(PWB.Password))
+            {
+                MessageBox.Show("Введите логин и пароль");
+                return;
+            }
+
+            using (var db = new SAVSAA_Material_storageEntities())
+            {
+                var user = db.Users.AsNoTracking().FirstOrDefault(u => u.Login == TLB.Text);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Данный логин не найден");
+                    return;
+                }
+
+                if (user.Password != PWB.Password)
+                {
+                    MessageBox.Show("Данный пароль неверен");
+                    return;
+                }
+
+                switch(user.IDRole)
+                {
+                    case 2:
+                        MessageBox.Show("Юзер");
+                        break;
+
+                    case 1:
+                        NavigationService?.Navigate(new CustomerMenu());
+                        break;
+                } 
+
+            }
+        }
+    }
+}
