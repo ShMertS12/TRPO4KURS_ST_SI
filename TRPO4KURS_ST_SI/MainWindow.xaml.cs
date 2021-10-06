b7a414819c33d922fd45410e1400b44fadb9732f
Александр Стецкий <186824@edu.fa.ru>
2021-10-06 16:22:28 +0300

    Сделанная_четвертая_практическая_работа_со_странным_выводом_Айдишников_и_Логинов

--- TRPO4KURS_ST_SI/MainWindow.xaml.cs
+++ TRPO4KURS_ST_SI/MainWindow.xaml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -25,7 +28,7 @@ namespace TRPO4KURS_ST_SI
         {
             InitializeComponent();
 
-
+            
             using (var db = new SAVSAA_Material_storageEntities1())
             {
                 var users = db.Users.AsNoTracking().ToList();
@@ -68,6 +71,69 @@ namespace TRPO4KURS_ST_SI
         private void ButCal_Click(object sender, RoutedEventArgs e)
         {
             MainFrame.Navigate(new Pr2_Calculator());
+        }
+
+       
+
+
+        private void ButEx_Click(object sender, RoutedEventArgs e)
+        {
+            /*string path = "export.txt";*/
+            
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.ShowDialog();
+            StreamWriter sw = new StreamWriter(sfd.FileName);
+
+            using (var db = new SAVSAA_Material_storageEntities1())
+            {
+                string IDline = string.Join(", ", db.Users.Select(x => x.ID));
+                string FIOline = string.Join(", ", db.Users.Select(x => x.FIO));
+                string LOGline = string.Join(", ", db.Users.Select(x => x.Login));
+                string PASSline = string.Join(", ", db.Users.Select(x => x.Password));
+                string RoleIDline = string.Join(", ", db.Users.Select(x => x.RoleID));
+                sw.WriteLine("ID: " + IDline);
+                sw.WriteLine("Fio: " + FIOline);
+                sw.WriteLine("Login: " + LOGline);
+                sw.WriteLine("Password: " + PASSline);
+                sw.WriteLine("Role: " + RoleIDline);
+                sw.Close();
+                Process.Start("notepad.exe", sfd.FileName);
+            }  
+        }
+
+        private void ButIm_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            if (ofd.FileName != "")
+            {
+                StreamReader sr = new StreamReader(ofd.FileName);
+                while (!sr.EndOfStream)
+                {
+                    string[] lines = new string[5];
+                    for (int i=0; i < 5; i++)
+                    {
+                        string line = sr.ReadLine();
+                        string[] data = line.Split(',');
+                        line = "";
+                        if(data.Length >= 2)
+                        {
+                            for(int j=0; j < data.Length; j++)
+                            {
+                                line += data[j] + ", ";
+                            }
+                            lines[i] = line ;
+                        }
+                        
+                    }
+                    MessageBox.Show("Данные в файле: \nID: " + lines[0] + 
+                        "\nФИО: " + lines[1] +  "\nЛогин: " + lines[2] +  "\nПароль: " +
+                        lines[3] +  "\nРоль: " + lines[4]);
+                }
+            }
+            else
+                MessageBox.Show("Файл не выбран");
         }
     }
 }
